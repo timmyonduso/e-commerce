06839c044ad82784c986ba206c84dae9a208578c
timmyonduso <timmyonduso58@gmail.com>
2024-02-10 20:38:19 +0300

    app rebuild

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,14 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: [
+    'prettier',
+    'simple-import-sort'
+  ],
   rules: {
     'prettier/prettier': 0,
     'no-console': 'off',
     "eol-last": "off",
+    'simple-import-sort/imports': 'error',
   },
 }
