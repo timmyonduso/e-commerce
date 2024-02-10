7eb44e1c0213f6dc6e55979261d129519c2fba00
timmyonduso <timmyonduso58@gmail.com>
2024-02-10 19:41:18 +0300

    app build

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': 0,
     'no-console': 'off',
   },
 }
